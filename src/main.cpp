0d3bf1d68a5aaf8da67eb578859983cbf04b6950
Tobi <frank.tobias@student.htlwrn.ac.at>
2021-01-05 17:28:39 +0100

    ergänze Funktion zum erstellen von Prozessen, weiterer Gedanke ->Rekursion

--- src/main.cpp
+++ src/main.cpp
@@ -7,32 +7,65 @@
 
 using namespace std;
 
-void print_process(){
+void print_process()
+{
   cout << "I am Prozess: " << getpid() << endl;
   cout << "parent id is " << getppid() << endl;
-  
+  cout << endl;
 }
+void fork_process(int n)
+{
+  int i = 0;
 
-int main(){
-
-  int i = 1;
-
-
-  while (i < 3)
+  while (i < n)
   {
     auto pid{fork()};
-    if (pid == -1){
+    if (pid == -1)
+    {
       cerr << "forking failed : " << errno << endl;
       exit(EXIT_FAILURE);
     }
 
-    if (pid == 0){
+    if (pid == 0)
+    {
       print_process();
       sleep(1);
       quick_exit(EXIT_SUCCESS);
     }
-    else {
-    
+    else
+    {
+      int status;
+      waitpid(pid, &status, 0);
+    }
+    i++;
+  }
+}
+
+int main()
+{
+  cout << "main process" << endl;
+  print_process();
+  int i = 1;
+
+  while (i < 3)
+  {
+    auto pid{fork()};
+    if (pid == -1)
+    {
+      cerr << "forking failed : " << errno << endl;
+      exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0)
+    {
+      print_process();
+      fork_process(2);
+      sleep(1);
+      quick_exit(EXIT_SUCCESS);
+    }
+    else
+    {
+
       int status;
       waitpid(pid, &status, 0);
       //cout << "child terminated w / exit code " << status << endl;
