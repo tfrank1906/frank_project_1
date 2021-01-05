1e4f04fd7d1c91869dcbd9981bed2a393a0ba457
Tobi <frank.tobias@student.htlwrn.ac.at>
2021-01-05 17:33:04 +0100

    ergänze Benutzerschnittstelle

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,23 @@
 #include <cerrno>
 #include <cstdlib>
 #include <sys/wait.h>
+#include <csignal>
+#include <vector>
 #include "CLI11.hpp"
 
 using namespace std;
 
+
 int cnt1 = 1;
 int cnt2 = 1;
-void print_process()
-{
+void print_process(){
   cout << "I am Prozess: " << getpid() << endl;
-  cout << "Parent id is " << getppid() << endl;
+  cout << "Parent id is : " << getppid() << endl;
   cout << endl;
 }
 void fork_process(int n, int m, int depth){
   int i = 0;
-  
-  
   if (depth == 0) {
-    
     while (i < n) {
       auto pid{fork()};
       if (pid == -1) {
@@ -32,7 +31,6 @@ void fork_process(int n, int m, int depth){
         cout << cnt1 << endl;
         print_process();
         fork_process(n, m, depth + 1);
-        sleep(1);
         quick_exit(EXIT_SUCCESS);
       } else {
         int status;
@@ -54,8 +52,7 @@ void fork_process(int n, int m, int depth){
         cout << cnt1 << "." << cnt2 << endl;
         print_process();
         fork_process(n, m, depth + 1);
-        sleep(1);
-        quick_exit(EXIT_SUCCESS);
+        //quick_exit(EXIT_SUCCESS);
       } else {
         int status;
         waitpid(pid, &status, 0);
@@ -67,8 +64,23 @@ void fork_process(int n, int m, int depth){
   }
 }
 
-int main() {
+int main(int argc, char** argv) {
+  CLI::App app{"Process Tree, project_1"};
+
+  int n;
+  int m;
+
+  app.add_option("children", n, "Number of children");
+  app.add_option("leaves", m, "Number of leaves");
+  CLI11_PARSE(app, argc, argv);
+
+ 
+
+ 
   cout << "main process" << endl;
   print_process();
-  fork_process(3, 2, 0);
+  fork_process(n, m, 0);
+
+  sleep(2);
+
 }
