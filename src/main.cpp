fcdcc940021d78101203485e9eddbbae89a895e2
Tobi <frank.tobias@student.htlwrn.ac.at>
2021-01-05 17:32:10 +0100

    refaktorisiere zurück auf 2 while - Schleifen, ergänze nummerierung der Prozesse laut Angabe

--- src/main.cpp
+++ src/main.cpp
@@ -7,43 +7,68 @@
 
 using namespace std;
 
-void print_process() {
+int cnt1 = 1;
+int cnt2 = 1;
+void print_process()
+{
   cout << "I am Prozess: " << getpid() << endl;
-  cout << "parent id is " << getppid() << endl;
+  cout << "Parent id is " << getppid() << endl;
   cout << endl;
 }
-void fork_process(int n, int m, int depth)
-{
-  int t = 0;
+void fork_process(int n, int m, int depth){
   int i = 0;
+  
+  
   if (depth == 0) {
-    t = n;
-  } else if (depth == 1) {
-    t = m;
+    
+    while (i < n) {
+      auto pid{fork()};
+      if (pid == -1) {
+        cerr << "forking failed : " << errno << endl;
+        exit(EXIT_FAILURE);
+      }
+      if (pid == 0) {
+        cnt1 += i;
+        cout << cnt1 << endl;
+        print_process();
+        fork_process(n, m, depth + 1);
+        sleep(1);
+        quick_exit(EXIT_SUCCESS);
+      } else {
+        int status;
+        waitpid(pid, &status, 0);
+      }
+      i++;
+    }
+  }
+  else if (depth == 1) {
+   
+    while (i < m) {
+      auto pid{fork()};
+      if (pid == -1) {
+        cerr << "forking failed : " << errno << endl;
+        exit(EXIT_FAILURE);
+      }
+      if (pid == 0) {
+        cnt2 += i;
+        cout << cnt1 << "." << cnt2 << endl;
+        print_process();
+        fork_process(n, m, depth + 1);
+        sleep(1);
+        quick_exit(EXIT_SUCCESS);
+      } else {
+        int status;
+        waitpid(pid, &status, 0);
+      }
+      i++;
+    }
   } else {
     return;
-  }
-  
-  while (i < t) {
-    auto pid{fork()};
-    if (pid == -1) {
-      cerr << "forking failed : " << errno << endl;
-      exit(EXIT_FAILURE);
-    }
-    if (pid == 0) {
-      print_process();
-      fork_process(n, m, depth + 1);
-      sleep(1);
-      quick_exit(EXIT_SUCCESS);
-    } else  {
-      int status;
-      waitpid(pid, &status, 0);
-    } i++;
   }
 }
 
 int main() {
   cout << "main process" << endl;
   print_process();
-  fork_process(2, 3, 0);
+  fork_process(3, 2, 0);
 }
