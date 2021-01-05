ff4cc48fa50610bb4ed12c0980f628487bd1305c
Tobi <frank.tobias@student.htlwrn.ac.at>
2021-01-05 17:30:42 +0100

    refaktorisiere Funktion aufgrund reduntanten Codes

--- src/main.cpp
+++ src/main.cpp
@@ -7,76 +7,42 @@
 
 using namespace std;
 
-void print_process()
-{
+void print_process() {
   cout << "I am Prozess: " << getpid() << endl;
   cout << "parent id is " << getppid() << endl;
   cout << endl;
 }
 void fork_process(int n, int m, int depth)
 {
-  if (depth == 0)
-  {
-    int i = 0;
-
-    while (i < n)
-    {
-      auto pid{fork()};
-      if (pid == -1)
-      {
-        cerr << "forking failed : " << errno << endl;
-        exit(EXIT_FAILURE);
-      }
-
-      if (pid == 0)
-      {
-        print_process();
-        fork_process(n, m, depth + 1);
-        sleep(1);
-        quick_exit(EXIT_SUCCESS);
-      }
-      else
-      {
-        int status;
-        waitpid(pid, &status, 0);
-      }
-      i++;
-    }
-  }
-  else if (depth == 1)
-  {
-    int i = 0;
-  
-    while (i < m)
-    {
-      auto pid{fork()};
-      if (pid == -1)
-      {
-        cerr << "forking failed : " << errno << endl;
-        exit(EXIT_FAILURE);
-      }
-
-      if (pid == 0)
-      {
-        print_process();
-        fork_process(n, m, depth + 1);
-        sleep(1);
-        quick_exit(EXIT_SUCCESS);
-      }
-      else
-      {
-        int status;
-        waitpid(pid, &status, 0);
-      }
-      i++;
-    }
+  int t = 0;
+  int i = 0;
+  if (depth == 0) {
+    t = n;
+  } else if (depth == 1) {
+    t = m;
   } else {
     return;
   }
+  
+  while (i < t) {
+    auto pid{fork()};
+    if (pid == -1) {
+      cerr << "forking failed : " << errno << endl;
+      exit(EXIT_FAILURE);
+    }
+    if (pid == 0) {
+      print_process();
+      fork_process(n, m, depth + 1);
+      sleep(1);
+      quick_exit(EXIT_SUCCESS);
+    } else  {
+      int status;
+      waitpid(pid, &status, 0);
+    } i++;
+  }
 }
 
-int main()
-{
+int main() {
   cout << "main process" << endl;
   print_process();
   fork_process(2, 3, 0);
