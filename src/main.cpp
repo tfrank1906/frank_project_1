533f6fcd35abe9fa9d09c938fa7e9263ebb91896
Tobi <frank.tobias@student.htlwrn.ac.at>
2021-01-06 15:07:05 +0100

    ergänze Speicherung der child pids um diese im nachhinein für die Signal e benutzen zu können

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <sys/wait.h>
 #include <csignal>
 #include <vector>
+#include <signal.h>
 #include "CLI11.hpp"
 
 using namespace std;
@@ -12,12 +13,15 @@ using namespace std;
 
 int cnt1 = 1;
 int cnt2 = 1;
+
 void print_process(){
   cout << "I am Prozess: " << getpid() << endl;
   cout << "Parent id is : " << getppid() << endl;
   cout << endl;
 }
 void fork_process(int n, int m, int depth){
+  vector<int> test;
+  
   int i = 0;
   if (depth == 0) {
     while (i < n) {
@@ -30,18 +34,32 @@ void fork_process(int n, int m, int depth){
         cnt1 += i;
         cout << cnt1 << endl;
         print_process();
+   
         fork_process(n, m, depth + 1);
         quick_exit(EXIT_SUCCESS);
       } else {
+        
+        test.push_back(pid);
+        
         int status;
+        cout << "var: " << pid << endl;
         waitpid(pid, &status, 0);
       }
+      
       i++;
     }
+    //main Prozess nach dem alle Prozesse erstellt wurden
+    cout << test.size() << endl;
+    for(size_t k = 0; k < test.size(); k++){
+      cout << test[k] << endl;
+    }
+    cout << getpid();
+  
+
+
   }
   else if (depth == 1) {
-   
-    while (i < m) {
+    while (i < m) { 
       auto pid{fork()};
       if (pid == -1) {
         cerr << "forking failed : " << errno << endl;
@@ -52,7 +70,7 @@ void fork_process(int n, int m, int depth){
         cout << cnt1 << "." << cnt2 << endl;
         print_process();
         fork_process(n, m, depth + 1);
-        //quick_exit(EXIT_SUCCESS);
+        quick_exit(EXIT_SUCCESS);
       } else {
         int status;
         waitpid(pid, &status, 0);
@@ -79,8 +97,23 @@ int main(int argc, char** argv) {
  
   cout << "main process" << endl;
   print_process();
+ 
   fork_process(n, m, 0);
-
+  /*
   sleep(2);
+  cout << children.size() << endl;
+  for(size_t c = 0; c < children.size(); c++){
+    cout << children.size() << endl;
+    cout << children[c] << endl;
+  }
+  int pid;
+  cout << "pid: ";
+  cin >> pid;
+
+  kill(pid, SIGKILL);
+  */
+
+
+
 
 }
