4430c992a9bd4c0a8632f2205c816dbbf19293b1
Tobi <frank.tobias@student.htlwrn.ac.at>
2021-01-07 15:08:24 +0100

    lösche kill_helper Funktion, ändere Signalhandler
    Funktionalität quasi vollständig bis auf einen Bug beim Löschen

--- src/main.cpp
+++ src/main.cpp
@@ -11,47 +11,41 @@
 
 using namespace std;
 
-
 int cnt1 = 1;
 int cnt2 = 1;
+vector<pid_t> children;
 
 
 
-void signalHandler(int signal){
-  cout << "signal erhalten " << signal << endl;
-
-    quick_exit(EXIT_SUCCESS);
-}
-
-void kill_helper(pid_t pid, int depth){
-  if(depth == 0){
-    int success = kill(pid, SIGTERM);
-    if(success == 0){
-      cout << "Kind " << pid << " wurde getötet!" << endl; 
-    } else {
-      cout << "Kind konnte nicht getötet werden." << endl;
+void signalHandler(int signal) {
+  cout << signal << endl;
+ 
+  if (children.size() > 0){
+    //cout << "muss noch eine ebene tiefer gehen!" << endl;
+    //cout << "ausgabe: " << children.size() << endl;
+    for(size_t c = 0; c < children.size(); c++){
+      cout << "kille : " << children[c] << endl;
+      int success = kill(children[c], SIGKILL);
+      cout << success << endl;
+      
     }
-  }  else if (depth == 1){
-    int success = kill(pid, SIGTERM);
-    //cout << success << endl;
-    if(success == 0){
-      cout << "Blatt " << pid << " wurde getötet!" << endl; 
-
-
-
   }
+  
+    cout << "kille nun: " << getpid() << endl;
+    
+    quick_exit(EXIT_SUCCESS);
+    
 }
-}
+
 void print_process(){
+
   cout << "I am Prozess: " << getpid() << endl;
   cout << "Parent id is : " << getppid() << endl;
   cout << endl;
-}
-void fork_process(int n, int m, int depth){
 
-  vector<pid_t> children;
-  vector<pid_t> leaves;
-  
+}
+
+void fork_process(int n, int m, int depth) {
   int i = 0;
   if (depth == 0) {
     while (i < n) {
@@ -65,32 +59,23 @@ void fork_process(int n, int m, int depth){
         cout << cnt1 << endl;
         print_process();
         fork_process(n, m, depth + 1);
-        sleep(5);
-        //quick_exit(EXIT_SUCCESS);
-      } else { 
-        children.push_back(pid);
-        /*
-        int status;
-      
-        waitpid(pid, &status, 0);
-        */
+        sleep(50);
       }
-      
+      else {
+        children.push_back(pid);
+      }
+      sleep(1);
       i++;
     }
-    //main Prozess nach dem alle Prozesse erstellt wurden
-    int status;
     sleep(2);
-    for(size_t k = 0; k < children.size(); k++){
-     
-      kill_helper(children[k], depth);
-      cout << children[k] << endl;
-      waitpid(children[k], &status, 0);
+    int status;
+    for (size_t k = 0; k < children.size(); k++) {
+      kill(children[k], SIGTERM);
+      waitpid(children[k], &status, 0);  
     }
-
   }
   else if (depth == 1) {
-    while (i < m) { 
+    while (i < m) {
       auto pid{fork()};
       if (pid == -1) {
         cerr << "forking failed : " << errno << endl;
@@ -100,65 +85,36 @@ void fork_process(int n, int m, int depth){
         cnt2 += i;
         cout << cnt1 << "." << cnt2 << endl;
         print_process();
-        //quick_exit(EXIT_SUCCESS);
-      } else {
-        /*
-        int status;
-        waitpid(pid, &status, 0);
-        */
-        leaves.push_back(pid);
+        sleep(50);
+      }
+      else {
+        children.push_back(pid);
       }
       i++;
-      
     }
-
-/*
-    for(size_t k = 0; k < leaves.size(); k++){
-      //kill_helper(leaves[k], depth);
-      cout << leaves[k] << endl;
-      waitpid(leaves[k], &status, 0);
-    }
-    */
-  } else {
+    sleep(1);
+  }
+  else
+  {
     return;
   }
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   signal(SIGTERM, signalHandler);
-  
+ 
   CLI::App app{"Process Tree, project_1"};
 
   int n;
   int m;
-  
 
   app.add_option("children", n, "Number of children");
   app.add_option("leaves", m, "Number of leaves");
   CLI11_PARSE(app, argc, argv);
 
- 
-
- 
   cout << "main process" << endl;
   print_process();
- 
+
   fork_process(n, m, 0);
-  /*
-  sleep(2);
-  cout << children.size() << endl;
-  for(size_t c = 0; c < children.size(); c++){
-    cout << children.size() << endl;
-    cout << children[c] << endl;
-  }
-  int pid;
-  cout << "pid: ";
-  cin >> pid;
-
-  kill(pid, SIGKILL);
-  */
-
-
-
 
 }
